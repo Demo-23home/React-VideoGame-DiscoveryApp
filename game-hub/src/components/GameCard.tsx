5529427eee4ae7e367601f00d831e39f4e6e974d
demo-23home <zeyadslama23@gmail.com>
2023-12-07 00:53:53 +0200

    Displaying Critic-Score

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -1,19 +1,25 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
+import CriticScore from "./CriticScore";
 
 interface Props {
-  game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image}/>
-        <CardBody>
-          <Heading fontSize='xl'>{game.name}</Heading>
-          <PlatformIconsList platforms = {game.parent_platforms.map(p => p.platform)} />
-        </CardBody>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="xl">{game.name}</Heading>
+        <HStack justifyContent='space-between'>
+        <PlatformIconsList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+        <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
     </Card>
   );
 };
