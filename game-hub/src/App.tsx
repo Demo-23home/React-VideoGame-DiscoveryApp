e1231e06492fd3c4ebe34a8903bc42b869008c32
demo-23home <zeyadslama23@gmail.com>
2023-12-06 16:30:54 +0200

    Installing Chakara UI

---
+++ game-hub/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function App() {
+  
+  return (
+    <div >
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  )
+}
+
+export default App
