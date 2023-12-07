2188d1794bbd0e3b8b808c7d35b1522d416e6c51
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2023-12-07 05:26:14 +0200

    Sort the games

--- game-hub/src/services/image-url.ts
+++ game-hub/src/services/image-url.ts
@@ -1,5 +1,6 @@
 
 const getCroppedImage = (url:string) => {
+    if (!url) return '';
     const target = 'media/'.length;
     const index = url.indexOf('media/') + target;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index); 
