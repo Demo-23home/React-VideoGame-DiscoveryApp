99b83d887c779c0cdf749a9c73bf6da190d78c1f
demo-23home <zeyadslama23@gmail.com>
2023-12-07 02:30:35 +0200

    Fetch The Genres

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,6 +1,8 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+
 function App() {
   return (
     <Grid templateAreas={{
@@ -12,7 +14,7 @@ function App() {
       </GridItem>
       <Show above="lg">
       <GridItem area="aside">
-        Aside
+        <GenreList/>
       </GridItem>
       </Show>
       <GridItem area="main">
---
+++ game-hub/src/components/GenreList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import useGenre from '../hooks/useGenres'
+
+
+const GenreList = () => {
+    const {genres} = useGenre();
+  return (
+    <ul>
+        {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+    </ul>
+  )
+}
+
+export default GenreList
